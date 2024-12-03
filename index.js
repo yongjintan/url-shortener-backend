915c0e0b9f5c5e6ed0e880e120647a08d1301207
yongjintan <yjtan@sg.palo-it.com>
2024-12-03 17:42:16 +0800

    add vercel.json

--- index.js
+++ index.js
@@ -113,5 +113,5 @@ app.get('/:shortString', async (req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running`);
 });
